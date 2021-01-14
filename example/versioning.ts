e174bd6eed5ffe1d73ea9bf29e67ff1b194e3675
Amin NAIRI <nairi.amin@gmail.com>
2021-01-14 12:08:59 +0100

    :tada: initial commit

---
+++ example/versioning.ts
@@ -0,0 +1,54 @@
+import {createHttpServer} from "@aminnairi/typescript-http-server";
+
+const {startHttpServer} = createHttpServer({
+  initialState: null,
+  middlewares: [],
+  routes: [
+    {
+      // http://127.0.0.1:8000/v1/ping
+      name: "Ping",
+      version: 1,
+      path: "/ping",
+      method: "GET",
+      response: () => ({
+        status: "OK",
+        headers: {
+          "Content-Type": "text/plain"
+        },
+        body: "pong"
+      })
+    },
+    {
+      // http://127.0.0.1:8000/v2/ping
+      name: "Ping",
+      version: 2,
+      path: "/ping",
+      method: "GET",
+      response: () => ({
+        status: "OK",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          data: "pong"
+        })
+      })
+    }
+  ],
+  fallback: {
+    status: "NOT_FOUND",
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: "Not found"
+  }
+});
+
+const main = async () => {
+  await startHttpServer({
+    port: 8000,
+    host: "127.0.0.1"
+  });
+
+  console.log("Server started on http://127.0.0.1:8000");
+};
