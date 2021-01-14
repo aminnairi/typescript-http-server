c833191d6bfde5d798706792016c9b887dc3b1fd
Amin NAIRI <nairi.amin@gmail.com>
2021-01-14 12:14:32 +0100

    :sparkles: calling the main function

--- example/simple.ts
+++ example/simple.ts
@@ -36,3 +36,5 @@ const main = async () => {
 
   console.log("Server started on http://127.0.0.1:8000");
 };
+
+main();
--- example/versioning.ts
+++ example/versioning.ts
@@ -52,3 +52,5 @@ const main = async () => {
 
   console.log("Server started on http://127.0.0.1:8000");
 };
+
+main();
