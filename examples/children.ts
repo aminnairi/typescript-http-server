a43fe4690e145357087090634bb96d8cba96c7e0
Amin NAIRI <nairi.amin@gmail.com>
2021-01-15 19:04:43 +0100

    :pencil2: updated the example to a more relevant one

--- examples/children.ts
+++ examples/children.ts
@@ -1,43 +1,79 @@
-import {createHttpServer} from "@aminnairi/typescript-http-server";
+import {createHttpServer} from "../index";
 
 const {startHttpServer} = createHttpServer({
   initialState: null,
   middlewares: [],
   routes: [
     {
-      // http://127.0.0.1:8000/v1/ping
-      name: "Ping",
+      // http://127.0.0.1:8000/v1/users
+      name: "Users",
       prefix: "",
       version: 1,
-      path: "/ping",
+      path: "/users",
       method: "GET",
       middlewares: [],
-      children: [
-        {
-          http://127.0.0.1:8000/v1/ping/pong
-          name: "Ping",
-          prefix: "",
-          version: 1,
-          path: "/pong",
-          method: "GET",
-          middlewares: [],
-          children: [],
-          response: () => ({
-            status: "OK",
-            headers: {
-              "Content-Type": "text/plain"
-            },
-            body: "pingpong"
-          })
-        }
-      ],
       response: () => ({
         status: "OK",
         headers: {
           "Content-Type": "text/plain"
         },
-        body: "ping"
-      })
+        body: "Users"
+      }),
+      children: [
+        {
+          // http://127.0.0.1:8000/v1/users/15
+          name: "User",
+          prefix: "",
+          version: 1,
+          path: ":user",
+          method: "GET",
+          middlewares: [],
+          response: ({parameters: {user}}) => ({
+            status: "OK",
+            headers: {
+              "Content-Type": "text/plain"
+            },
+            body: `User#${String(user)}`
+          }),
+          children: [
+            {
+              // http://127.0.0.1:8000/v1/users/15/posts
+              name: "User's posts",
+              prefix: "",
+              version: 1,
+              path: "/posts",
+              method: "GET",
+              middlewares: [],
+              response: ({parameters: {user}}) => ({
+                status: "OK",
+                headers: {
+                  "Content-Type": "text/plain"
+                },
+                body: `Users#${String(user)}'s posts`
+              }),
+              children: [
+                {
+                  // http://127.0.0.1:8000/v1/users/15/posts/3
+                  name: "User's posts",
+                  prefix: "",
+                  version: 1,
+                  path: ":post",
+                  method: "GET",
+                  middlewares: [],
+                  children: [],
+                  response: ({parameters: {user, post}}) => ({
+                    status: "OK",
+                    headers: {
+                      "Content-Type": "text/plain"
+                    },
+                    body: `User#${String(user)}'s post#${String(post)}`
+                  })
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   ],
   fallback: () => ({
