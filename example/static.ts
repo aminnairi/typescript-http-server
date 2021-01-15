9b0a1cbc1f9994e717ea28891e77c292aa94e995
Amin NAIRI <nairi.amin@gmail.com>
2021-01-15 10:11:51 +0100

    :tada: initial commit

---
+++ example/static.ts
@@ -0,0 +1,66 @@
+import {createHttpServer} from "../main";
+import {promises as fs} from "fs";
+import {resolve} from "path";
+
+const {startHttpServer} = createHttpServer({
+  initialState: null,
+  middlewares: [],
+  routes: [
+    {
+      // http://127.0.0.1:8000/static/async.ts
+      // http://127.0.0.1:8000/static/simple.ts
+      // http://127.0.0.1:8000/static/parameters.ts
+      name: "Static file serving (example folder)",
+      prefix: "/static",
+      version: 0,
+      path: ":file",
+      method: "GET",
+      middlewares: [],
+      response: async ({parameters: {file}}) => {
+        try {
+          const fileName = file || "";
+          const filePath = resolve(__dirname, fileName);
+          const fileContent = await fs.readFile(filePath);
+
+          return {
+            status: "OK",
+            headers: {
+              "Content-Type": "text/plain"
+            },
+            body: fileContent.toString()
+          };
+        } catch {
+          return {
+            status: "NOT_FOUND",
+            headers: {
+              "Content-Type": "text/plain"
+            },
+            body: `File ${file || "unknown"} is not found in the example folder.`
+          };
+        }
+
+      }
+    }
+  ],
+  fallback: () => ({
+    status: "NOT_FOUND",
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: "Not found"
+  })
+});
+
+const main = async () => {
+  const port = 8000;
+  const host = "127.0.0.1";
+
+  await startHttpServer({
+    port,
+    host
+  });
+
+  console.log(`Server started on http://${host}:${port}`);
+};
+
+main().catch(console.error);
